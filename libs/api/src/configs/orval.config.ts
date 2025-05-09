94cac069a341c9f687367a6d095efad6d973bf3e
Farshid <farenjir@gmail.com>
2025-05-09 14:08:36 +0330

    init-api

--- libs/api/src/configs/orval.config.ts
+++ libs/api/src/configs/orval.config.ts
@@ -1,28 +1,27 @@
 const configs = {
   default: {
     output: {
-      mode: 'tags-split',
-      target: '../generated/api.ts',
-      schemas: '../generated/schemas.ts',
+      target: '../generated/api',
+      schemas: '../generated/schemas',
       client: 'react-query',
-      mock: true,
+      prettier: true,
       override: {
         mutator: {
           path: './axios.config.ts',
           name: 'customAxios',
         },
-        // query: {
-        //   useQuery: true,
-        //   useInfinite: true,
-        //   useInfiniteQueryParam: 'nextId',
-        //   options: {
-        //     staleTime: 10000,
-        //   },
-        // },
+        query: {
+          useQuery: true,
+          useInfinite: true,
+          useInfiniteQueryParam: 'nextId',
+          options: {
+            staleTime: 10000,
+          },
+        },
       },
     },
     input: {
-      target: './services/index.yaml',
+      target: '../services/index.json',
     },
   },
 };
