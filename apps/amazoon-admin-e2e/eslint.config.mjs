86850336d72a8c8d597bf93f13a372d1e9f991d6
Farshid <farenjir@gmail.com>
2025-05-09 12:38:51 +0330

    init-utils

--- apps/amazoon-admin-e2e/eslint.config.mjs
+++ apps/amazoon-admin-e2e/eslint.config.mjs
@@ -1,7 +1,9 @@
+import baseConfig from '../../eslint.base.config.mjs';
 import cypress from 'eslint-plugin-cypress/flat';
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
+  ...baseConfig,
   cypress.configs['recommended'],
   ...baseConfig,
   {
